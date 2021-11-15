946e8a1a6dba0c4b7727c6d2521e608361c70eed
Sumit Chahar <chahar.sumit888@gmail.com>
2021-11-15 23:41:39 +0530

    Complete Image App

--- block-BJaaju/code/index.js
+++ block-BJaaju/code/index.js
@@ -1,11 +1,12 @@
 let input = document.querySelector('input');
 let allImages = Array.from(document.querySelectorAll('.gallery-item img'));
-
+let imageGallery = document.querySelector('.image-gallery');
 input.addEventListener('keyup', handleInput);
 
 function handleInput(event){
-  let imageKey = event.target.value;
+  let imageKey = input.value;
   if(event.key === 'Enter'){
+    imageGallery.innerHTML = '';
     handleImageCreation(imageKey);
     event.target.value = '';
   }
@@ -13,7 +14,21 @@ function handleInput(event){
 
 function handleImageCreation(key){
   let xhr = new XMLHttpRequest();
-  xhr.open('GET', `https://source.unsplash.com/featured/?grafiti`);
-  let imageData = xhr.response;
-  console.log(xhr);
+  xhr.open('GET', `https://api.unsplash.com/search/photos?page=1&query=${key}&client_id=OYOTItSH5_c56O-OArkzV_irhLKOFV3Yc-Gz73gBMkA`);
+  xhr.onload = function(){
+    let imageData = JSON.parse(xhr.response);
+    imageData.results.forEach(elm => {
+    handleUI(elm.urls.raw);
+    });
+  }
+  xhr.send();
+}
+
+function handleUI(image){
+  let div = document.createElement('div');
+  div.classList.add('gallery-item');
+  let img = document.createElement('img');
+  img.src = image;
+  div.append(img);
+  imageGallery.append(div);
 }
