6507dffdb157e4be854a67519cd4a2005db7785c
Sumit Chahar <chahar.sumit888@gmail.com>
2021-11-16 20:32:52 +0530

    Complete Assignment Promises

---
+++ block-BJaakb/code/index.js
@@ -0,0 +1,13 @@
+let url = 'https://api.github.com/users/chaharsumit';
+
+function fetch(url){
+  return new Promise((resolve, reject) => {
+    let xhr = new XMLHttpRequest();
+    xhr.open('GET', url);
+    xhr.onload = () => resolve(JSON.parse(xhr.response));
+    xhr.onerror = () => reject('Something went wrong!');
+    xhr.send();
+  })
+}
+
+let data = fetch(url).then((data) => {console.log('Fetching data successful')}).catch((error) => {'ERROR 404'});
